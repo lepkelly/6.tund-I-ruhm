51dcbad0b3fcae61d12abbb1a95aa71c71b5e87c
Kelly Leppik <kelllep@greeny.cs.tlu.ee>
2015-10-10 17:29:17 +0300

    lisatud mõned puuduvad read

--- functions.php
+++ functions.php
@@ -4,45 +4,57 @@
     function getAllData(){
         
         $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plate WHERE deleted IS NULL");
         $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
         $stmt->execute();
 		
-		        
-        $row_nr = 0;
-		
-		echo "<table border=10>";
-		echo "<tr><th>rea nr</th><th>auto nr märk</th></tr>"
-		
-		
+		//massiiv kus hoiame autosid
+		$array = array();
 		
         // iga rea kohta mis on ab'is teeme midagi
         while($stmt->fetch()){
-            //saime andmed kätte
-            echo $row_nr." ".$number_plate_from_db." <br>";
-            //echo $row_nr." ".$number_plate_from_db." <br>";
+            //suvaline muutuja, kus hoiame auto andmeid selle hetkeni kui lisame massiivi
 			
-			//uus rida
-			echo "<tr>";
-			//reanr tulba
-			echo "<td>".$row_nr."</td>"
-			//auto nr tulba
-			echo "<td>" .$number_plate_from_db."</td>"
-			//panete rea kinni
-			echo "</tr";
-		
-			$row_nr++;
-        }
-        
-        // iga rea kohta mis on ab'is teeme midagi
-        while($stmt->fetch()){
-            //saime andmed kätte
-            echo($user_id_from_db);
-            //? kuidas saada massiivi - SIIT JÄTKAME
-        }
+			//tühi objekt kus hoiame väärtusi
+			$car = new StdClass();
+			
+			$car->id = $id_from_db;
+			$car->number_plate = $number_plate_from_db;
+			$car->user_id = $user_id_from_db; 
+            $car->color = $color_from_db;
+			
+			//ilsan massiivi (auto lisan massiivi)
+			array_push($array, $car);
+			//echo "<pre>";
+			//var_dump($array);
+			//echo "</pre>";
+			
+		}
+		//auto lisan massiivi
+		return $array;
+		//echo "<pre>";
+		//var_dump($array);
+		//echo "</pre>";
         
         $stmt->close();
         $mysqli->close();
+    }
+	
+	function deleteCarData($car_id){
+        
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        
+        // uuendan välja deleted, lisan praeguse date'i
+        $stmt = $mysqli->prepare("UPDATE car_plate SET deleted=NOW() WHERE id=?");
+        $stmt->bind_param("i", $car_id);
+        $stmt->execute();
+        
+        // tühjendame aadressirea
+        header("Location: table.php");
+        
+        $stmt->close();
+        $mysqli->close();
+        
     }
     
     
